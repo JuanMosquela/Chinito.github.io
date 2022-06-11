ef2359634fd07677a35c6fe2f6d36fa21a2234d7
Juan Mosquella <jmosquella11@gmail.com>
2022-06-11 17:10:37 -0300

    Web terminada

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const $question = document.querySelectorAll(".container .container-question");
+const questions = document.querySelectorAll(".container-question");
 const menu = document.querySelector('.menu');
 const latNav = document.getElementById('lat-nav');
-const header = document.querySelector(".header"); 
+const header = document.querySelector(".header");
+const icons = document.querySelectorAll('.question i');
 
-$question.forEach(el => {
-    el.addEventListener('click', ()=>{
-        el.classList.toggle('is-active');        
+
+
+
+
+
+questions.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('active')
     })
-    
-});
+})
 
 window.addEventListener('scroll', ()=>{
            
     header.classList.toggle('sticky', window.scrollY > 0);
     if(latNav.classList.contains('open-close')){
         header.classList.add('sticky')
-
-    } 
-    
-    
-    
+    }     
 
 })
 
